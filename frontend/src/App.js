44f301b27dbdcf528ab2b1a7ba3761d6a83c7962
Joakim Riikonen <joakim.riiikonen@gmail.com>
2021-01-11 13:41:46 +0200

    products page styling done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,25 +7,32 @@ import {
 import Navbar from './components/Navbar'
 import FrontPage from './components/FrontPage'
 import ProductsPage from './components/ProductsPage'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  font-family: Helvetica;
+`
 
 const App = () => {
   return(
-    <Router>
-      <Navbar/>
-      <Switch>
-        <Route exact path='/' render={() => (
-          <FrontPage/>
-        )}/>
-        <Route exact path='/products/:category' render={({ match }) => (
-          <ProductsPage category={match.params.category}/>
-        )}/>
-        <Route>
-          <div>
-            Page not found
-          </div>
-        </Route>
-      </Switch>
-    </Router>
+    <Container>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path='/' render={() => (
+            <FrontPage/>
+          )}/>
+          <Route exact path='/products/:category' render={({ match }) => (
+            <ProductsPage category={match.params.category}/>
+          )}/>
+          <Route>
+            <div>
+              Page not found
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    </Container>
   )
 } 
 
--- frontend/src/components/ProductsPage.js
+++ frontend/src/components/ProductsPage.js
@@ -1,5 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import productsService from '../services/products'
+import styled from 'styled-components'
+
+const PageContainer = styled.div`
+  text-align: center;
+`
+
+const ProductTitle = styled.h1`
+  font-size: 45px;
+`
+
+const ItemContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 60%;
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 2px;
+  margin: 4px auto;
+  font-size: 20px;
+`
+
+const ItemAttribute = styled.span`
+  padding: 0 10px;
+`
+
+const ItemName = styled(ItemAttribute)`
+  font-weight: bold;
+`
+
+const ItemManufacturer = styled(ItemAttribute)`
+  text-transform: capitalize;
+`
+
+const InStockDisplay = styled.span`
+  color: green;
+`
+
+const LessThan10Display = styled.span`
+  color: orange;
+`
+
+const OutOfStockDisplay = styled.span`
+  color: red;
+`
 
 const ProductsPage = ({ category }) => {
 
@@ -13,29 +57,54 @@ const ProductsPage = ({ category }) => {
         setProducts(data)
       })
   }, [category])
+
+  /*
+    takes the raw instockvalue of a product as a parameter and
+    returns a properly formatted div
+  */
+  const inStockFormat = (inStockValue) => {
+    switch(inStockValue){
+      case "INSTOCK":
+        return(<InStockDisplay>IN STOCK</InStockDisplay>)
+      case "LESSTHAN10":
+        return(<LessThan10Display>LESS THAN 10</LessThan10Display>)
+      case "OUTOFSTOCK":
+        return(<OutOfStockDisplay>OUT OF STOCK</OutOfStockDisplay>)
+      default:
+        return(<></>)
+    }
+  }
   
   return(
-    <div>
-      <h1>{category}</h1>
-      {products.map((product, i) => {
-        return (
-          <div key={i}>
-            {product.name}
-            <br/>
-            colors:
-            {product.color.map((c) => (c))}
-            <br/>
-            {product.manufacturer}
-            <br/>
-            {product.price}€
-            <br/>
-            {product.instockvalue}
-            <br/>
-            <br/>
-          </div>
-        )
-      })}
-    </div>
+    <PageContainer>
+      <ProductTitle>{category.toUpperCase()}</ProductTitle>
+      <div>
+        {products.map((product, i) => {
+          return (
+            <ItemContainer key={i}>
+              <div>
+                <ItemName>{product.name}</ItemName>
+                <ItemManufacturer>
+                  Manufacturer: 
+                  {" " + product.manufacturer}
+                </ItemManufacturer>
+                <ItemAttribute>
+                  Colors:
+                  {product.color.map((c, j) => (
+                    (j < product.color.length-1) ? (" " + c + ',') : (" " + c))
+                  )}
+                </ItemAttribute>
+                <ItemAttribute>
+                  Price:
+                  {" " + product.price}€
+                </ItemAttribute>
+                </div>
+              {inStockFormat(product.instockvalue)}
+            </ItemContainer>
+          )
+        })}
+      </div>
+    </PageContainer>
   )
 }
 
