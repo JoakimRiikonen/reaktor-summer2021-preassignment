cf352acc8eb06fec699c1b0c0e222bd76b69d106
Joakim Riikonen <joakim.riiikonen@gmail.com>
2021-01-11 12:41:35 +0200

    added navigation bar styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  text-align: center;
+  border-bottom: 2px solid black;
+  padding-top: 3px;
+  padding-bottom: 10px;
+  margin: 0 10px;
+`
+
+const StyledLink = styled(Link)`
+  display: inline-block;
+  padding: 0 10px;
+  margin: 0 5px;
+  font-size: 30px;
+  font-weight: bold;
+  color: black;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+`
 
 const Navbar = () => {
   return(
-    <div>
-      <Link to='/products/gloves'>Gloves</Link>
-      <Link to='/products/facemasks'>Facemasks</Link>
-      <Link to='/products/beanies'>Beanies</Link>
-    </div>
+    <Container>
+      <StyledLink to='/products/gloves'>Gloves</StyledLink>
+      <StyledLink to='/products/facemasks'>Facemasks</StyledLink>
+      <StyledLink to='/products/beanies'>Beanies</StyledLink>
+    </Container>
   )
 }
 
