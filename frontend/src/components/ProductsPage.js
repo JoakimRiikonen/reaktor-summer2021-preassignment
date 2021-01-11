7214ba3cc4e3e0fbce34f28f3b6f3285739ab45c
Joakim Riikonen <joakim.riiikonen@gmail.com>
2021-01-11 12:18:20 +0200

    added simple product listing on product pages

--- frontend/src/components/ProductsPage.js
+++ frontend/src/components/ProductsPage.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import productsService from '../services/products'
 
-const ProductsPage = (props) => {
+const ProductsPage = ({ category }) => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    productsService
+      .getProducts(category)
+      .then((data) => {
+        setProducts(data)
+      })
+  }, [])
   
   return(
     <div>
-      Products
+      <h1>{category}</h1>
+      {products.map((product, i) => {
+        return (
+          <div key={i}>
+            {product.name}
+            <br/>
+            colors:
+            {product.color.map((c) => (c))}
+            <br/>
+            {product.manufacturer}
+            <br/>
+            {product.price}€
+            <br/>
+            {product.instockvalue}
+            <br/>
+            <br/>
+          </div>
+        )
+      })}
     </div>
   )
 }
