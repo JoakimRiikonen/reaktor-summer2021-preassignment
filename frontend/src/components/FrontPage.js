d1e7317acd25db442df7cfdb0c425821c4278a45
Joakim Riikonen <joakim.riiikonen@gmail.com>
2021-01-11 11:57:43 +0200

    added links to categories to the frontpage

--- frontend/src/components/FrontPage.js
+++ frontend/src/components/FrontPage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const FrontPage = () => {
   return(
     <div>
       Frontpage
+      <br/>
+      <Link to='/products/gloves'>Gloves</Link>
+      <br/>
+      <Link to='/products/facemasks'>Facemasks</Link>
+      <br/>
+      <Link to='/products/beanies'>Beanies</Link>
     </div>
   )
 }
